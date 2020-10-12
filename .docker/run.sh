3ea1e2718b4cccde1c6c83160928afa4523e65a3
Thomas Schulte <thomas@cupracer.de>
2020-10-12 16:15:55 +0200

    fix sed command

--- .docker/run.sh
+++ .docker/run.sh
@@ -4,7 +4,7 @@ if [ -z $ALLOWED_HOSTS ]; then
   ALLOWED_HOSTS="['*']"
 fi
 
-sed -E -i 's/^(ALLOWED_HOSTS.*=).*$/\1 '${ALLOWED_HOSTS}'/' family_tools/settings.py
+sed -E -i "s/^(ALLOWED_HOSTS.*=).*$/\1 '${ALLOWED_HOSTS}'/" family_tools/settings.py
 
 ./manage.py migrate
 ./manage.py runserver 0.0.0.0:8000
