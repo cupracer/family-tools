45af7eed74bc7f7ef733af8239be7098bc8ae367
Thomas Schulte <thomas@cupracer.de>
2020-10-12 08:45:40 +0200

    use data migration to create default members group with permissions

--- main/apps.py
+++ main/apps.py
@@ -1,10 +1,5 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
 
 
 class FamilyToolsConfig(AppConfig):
     name = 'main'
-
-    def ready(self):
-        from .signals import populate_models
-        post_migrate.connect(populate_models, sender=self)
--- main/migrations/0002_create-members-group-with-permissions.py
+++ main/migrations/0002_create-members-group-with-permissions.py
@@ -1,10 +1,13 @@
-from django.contrib.auth.models import Group, Permission
-from django.contrib.contenttypes.models import ContentType
-
+from django.core.management.sql import emit_post_migrate_signal
+from django.db import migrations
 from housekeeping_book.models import AccountHolder, Category, Booking, PeriodicBooking
 
 
-def populate_models(sender, **kwargs):
+def make_permissions(apps,schema_editor):
+    emit_post_migrate_signal(2, False, 'default')
+    Group = apps.get_model("auth", "Group")
+    Permission = apps.get_model("auth", "Permission")
+    ContentType = apps.get_model('contenttypes', 'ContentType')
     member, created = Group.objects.get_or_create(name='members')
 
     # account_holder
@@ -70,3 +73,14 @@ def populate_models(sender, **kwargs):
             codename='add_periodicbooking'
         )
     )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0001_initial'),
+        ('housekeeping_book', '0002_auto_20201012_0537'),
+    ]
+
+    operations = [
+        migrations.RunPython(make_permissions, reverse_code=lambda *args, **kwargs: True)
+    ]
