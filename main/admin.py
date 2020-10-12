21101a520b5ec82a4042aaf593dd1d090ff18f4e
Thomas Schulte <thomas@cupracer.de>
2020-10-12 08:11:38 +0200

    use Group instead of Team entity

--- main/admin.py
+++ main/admin.py
@@ -2,18 +2,18 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from guardian.admin import GuardedModelAdmin
 
-from main.models import Team, User
+from main.models import User
 
 
 class UserAdmin(GuardedModelAdmin):
     pass
 
 
-class TeamAdmin(GuardedModelAdmin):
+class GroupAdmin(GuardedModelAdmin):
     pass
 
 
 admin.site.unregister(Group)
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Team, TeamAdmin)
+admin.site.register(Group, GroupAdmin)
