26c14a2630347fe1071bbb08a8382bd378923bf0
Thomas Schulte <thomas@cupracer.de>
2020-10-12 18:24:45 +0200

    disable debugging

--- .docker/run.sh
+++ .docker/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ -z $ALLOWED_HOSTS ]; then
+if [ -z "$ALLOWED_HOSTS" ]; then
   ALLOWED_HOSTS="['*']"
 fi
-
-sed -E -i "s/^(ALLOWED_HOSTS.*=).*$/\1 ${ALLOWED_HOSTS}/" family_tools/settings.py
+sed -E -i 's/^DEBUG[ ]*=.*/DEBUG = False/' family_tools/settings.py
+sed -E -i "s/^ALLOWED_HOSTS[ ]*=.*$/ALLOWED_HOSTS = ${ALLOWED_HOSTS}/" family_tools/settings.py
 
 ./manage.py migrate
-./manage.py runserver 0.0.0.0:8000
+./manage.py runserver --insecure 0.0.0.0:8000
