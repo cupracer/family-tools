e6973aa36446f6ff13a11c8ce47c8c0e9ebafbeb
Thomas Schulte <thomas@cupracer.de>
2020-10-12 15:45:27 +0200

    make ALLOWED_HOSTS configurable

--- .docker/run.sh
+++ .docker/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ -z $ALLOWED_HOSTS ]; then
+  ALLOWED_HOSTS="['*']"
+fi
+
+sed -E -i 's/^(ALLOWED_HOSTS.*=).*$/\1 '${ALLOWED_HOSTS}'/' family_tools/settings.py
+
 ./manage.py migrate
 ./manage.py runserver 0.0.0.0:8000
