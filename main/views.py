f1d6ee3104d231cbd131b2aebab7b6dbcfcc660f
Thomas Schulte <thomas@cupracer.de>
2020-10-12 22:36:08 +0200

    force login on index page

--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 from django.shortcuts import render
 
 
-class IndexView(TemplateView):
+class IndexView(LoginRequiredMixin, TemplateView):
     template_name = "main/base.html"
     site = {
         'name': 'FamilyTools',
