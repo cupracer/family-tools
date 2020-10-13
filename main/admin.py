dc29c9aaa63bc00554673c181caa972fadc4f674
Thomas Schulte <thomas@cupracer.de>
2020-10-13 06:49:40 +0200

    switch plain admin classes to Guardian

--- main/admin.py
+++ main/admin.py
@@ -1,19 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from django.contrib.auth.models import Group
-from guardian.admin import GuardedModelAdmin
+from guardian.admin import GuardedModelAdminMixin
 
 from main.models import User
 
 
-class UserAdmin(GuardedModelAdmin):
+class GuardedUserAdmin(GuardedModelAdminMixin, UserAdmin):
     pass
 
 
-class GroupAdmin(GuardedModelAdmin):
+class GuardedGroupAdmin(GuardedModelAdminMixin, GroupAdmin):
     pass
 
 
 admin.site.unregister(Group)
 
-admin.site.register(User, UserAdmin)
-admin.site.register(Group, GroupAdmin)
+admin.site.register(User, GuardedUserAdmin)
+admin.site.register(Group, GuardedGroupAdmin)
