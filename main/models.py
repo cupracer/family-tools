f4a456875eae8f9f53df58d00fd07c63db06dba6
Thomas Schulte <thomas@cupracer.de>
2020-10-13 09:29:21 +0200

    return full name if available

--- main/models.py
+++ main/models.py
@@ -2,4 +2,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        if self.get_full_name():
+            return self.get_full_name()
+        else:
+            return self.get_username()
