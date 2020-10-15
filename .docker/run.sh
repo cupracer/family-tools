39a81cf9c4f4deadf6630eb709e4c352c46b950c
Thomas Schulte <thomas@cupracer.de>
2020-10-15 07:01:51 +0200

    just adds a blank line

--- .docker/run.sh
+++ .docker/run.sh
@@ -3,6 +3,7 @@
 if [ -z "$ALLOWED_HOSTS" ]; then
   ALLOWED_HOSTS="['*']"
 fi
+
 sed -E -i 's/^DEBUG[ ]*=.*/DEBUG = False/' family_tools/settings.py
 sed -E -i "s/^ALLOWED_HOSTS[ ]*=.*$/ALLOWED_HOSTS = ${ALLOWED_HOSTS}/" family_tools/settings.py
 
