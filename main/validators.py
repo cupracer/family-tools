593e04355826657af35df1f5c7772a642e2a170c
Thomas Schulte <thomas@cupracer.de>
2020-11-11 07:24:50 +0100

    add allowed characters

--- main/validators.py
+++ main/validators.py
@@ -3,7 +3,7 @@ import re
 
 
 def validate_item_name(value):
-    p = re.compile('^[a-züöäßA-ZÜÖÄ0-9][ a-züöäßA-ZÜÖÄ0-9-]*[a-züöäßA-ZÜÖÄ0-9]$')
+    p = re.compile('^[a-züöäßA-ZÜÖÄ0-9][ a-züöäßA-ZÜÖÄ0-9-\'!]*[a-züöäßA-ZÜÖÄ0-9\'!]$')
     if not p.match(value):
         raise ValidationError("A valid item name must be entered in")
     else:
